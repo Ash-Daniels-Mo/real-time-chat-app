6be935c054f5ae613eda93ce87891987e14f8da7
elgenio <rickentchabet@gmail.com>
2026-01-05 11:58:35 +0100

    edit run.py for deployment

--- realtime-chat-backend/run.py
+++ realtime-chat-backend/run.py
@@ -6,5 +6,5 @@ from app.extensions import socketio
 
 app = create_app()
 
-if __name__ == "__main__":
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+#     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
